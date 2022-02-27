64f92c63cf93b7bedf0e49f8803861ebbab907cb
QuantumCoded <bfields3@jscc.edu>
2022-02-26 22:05:27 -0600

    moving making quizlet requests to huntress because heroku won't do it

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import { google } from 'googleapis';
 import { createServer } from 'http';
 import { JSDOM } from 'jsdom';
-import * as superagent from 'superagent';
 import * as url from 'url'
-import { createReadStream } from 'fs';
 
 const search = google.customsearch('v1');
 
@@ -17,15 +15,32 @@ function google_search(query: string, callback: (error: any, result: any) => voi
 }
 
 createServer(async (req, res) => {
-    const results: any[] = [];
+    if (req.method === 'POST') {
+        let html = '';
 
-    if (req.url == undefined) {
-        res.writeHead(400, 'Bad Request').end();
+        req.on('data', chunk => {
+            html += chunk;
+
+            if (html.length > 1e7) {
+                req.socket.destroy();
+            }
+        });
+
+        req.on('end', () => {
+            const dom = new JSDOM(html);
+
+            const words: NodeListOf<HTMLSpanElement> = dom.window.document.querySelectorAll('.SetPageTerm-wordText > .TermText');
+            const definitions: NodeListOf<HTMLSpanElement> = dom.window.document.querySelectorAll('.SetPageTerm-definitionText > .TermText');
+            const terms = Array.from(words).map((word, i) => ({ word: word.textContent, definition: definitions[i].textContent }));
+
+            res.writeHead(200, 'OK', { 'content-type': 'text/json' }).end(JSON.stringify(terms));
+        });
+
         return;
     }
 
-    if (req.url == '/index.html' || req.url == '/') {
-        createReadStream('index.html').pipe(res.writeHead(200, 'OK'));
+    if (req.url == undefined) {
+        res.writeHead(400, 'Bad Request').end();
         return;
     }
 
@@ -36,44 +51,11 @@ createServer(async (req, res) => {
         return;
     }
 
-    google_search(search, async (error, result) => {
+    google_search(search, (error, result) => {
         if (error) {
-            if (!res.writableEnded) res.writeHead(500, 'Internal Server Error').end(JSON.stringify(error));
+            res.writeHead(500, 'Internal Server Error').end(JSON.stringify(error));
         } else {
-            const items: { title: string, link: string }[] = result?.data?.items;
-
-            await Promise.allSettled(items.map(async item => {
-                const { title, link } = item;
-
-                console.log("===================== BEFORE ERROR =====================");
-                const html = await new Promise<string>((resolve, reject) => {
-                    superagent
-                        .get(link)
-                        .set('Accept-Language', 'en-US,en;q=0.9,*;q=0.5')
-                        .set('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36')
-                        .end((err, res) => {
-                            if (err) {
-                                console.log(err);
-                                reject(err);
-                            } else {
-                                resolve(res.text);
-                            }
-                        });
-                });
-                console.log("===================== AFTER ERROR =====================")
-
-                const dom = new JSDOM(html);
-
-                if (dom) {
-                    const words: NodeListOf<HTMLSpanElement> = dom.window.document.querySelectorAll('.SetPageTerm-wordText > .TermText');
-                    const definitions: NodeListOf<HTMLSpanElement> = dom.window.document.querySelectorAll('.SetPageTerm-definitionText > .TermText');
-
-                    const map = Array.from(words).map((word, i) => ({ word: word.textContent, definition: definitions[i].textContent }));
-                    results.push({ title, link, terms: map });
-                }
-            }));
-
-            if (!res.writableEnded) res.writeHead(200, 'OK', { 'content-type': 'text/json' }).end(JSON.stringify(results));
+            res.writeHead(200, 'OK', { 'content-type': 'text/json' }).end(JSON.stringify(result?.data?.items));
         }
     });
 }).listen(process.env.PORT || 8080, () => { console.log("ready") });
